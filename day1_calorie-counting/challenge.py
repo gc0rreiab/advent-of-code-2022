4a2362eede229bbb73d4a134ab3cd4c75cbb6abe
gcb <gabriel.brito.95@gmail.com>
2022-12-04 14:15:44 +0000

    change file names

---
+++ day1_calorie-counting/challenge.py
@@ -0,0 +1,44 @@
+import sys
+
+class Elf:
+    def __init__(self, id, calories):
+        self.id = id
+        self.calories = calories
+    
+Elves = []
+
+file = open('input.txt', 'r')
+lines = file.readlines()
+current_id = 0
+current_calories = 0
+
+#create a list of Elves
+for line in lines:
+    if line == '\n':
+      elf =  Elf(current_id, current_calories)
+      Elves.append(elf)
+      current_id = current_id + 1
+      current_calories = 0
+    else:
+      current_calories = current_calories + int(line)
+
+#find the three Elves with the most calories
+topThree = []
+for elf in Elves:
+  if len(topThree) < 3:
+    topThree.append(elf)
+  elif elf.calories > topThree[2].calories:
+      topThree[2] = elf
+  topThree.sort(key=lambda x: x.calories, reverse=1)
+      
+#top three
+print("Top three: ")
+print(f'1: {topThree[0].calories}')
+print(f'2: {topThree[1].calories}')
+print(f'3: {topThree[2].calories}')
+
+#total
+print(f'Total: {topThree[0].calories + topThree[1].calories + topThree[2].calories}')
+
+exit()
+
---
+++ day2_rock-paper-scissors/challenge.py
@@ -0,0 +1,11 @@
+import sys
+
+file = open('input.txt', 'r')
+lines = file.readlines()
+
+for line in lines:
+    #code here
+
+
+
+exit()
