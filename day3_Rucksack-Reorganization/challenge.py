9b6f35a625d7754b6a97a4f80c67829fffa14eba
gcb <gabriel.brito.95@gmail.com>
2022-12-18 03:07:51 +0000

    Add part 1 solution

--- day3_Rucksack-Reorganization/challenge.py
+++ day3_Rucksack-Reorganization/challenge.py
@@ -1,2 +1,37 @@
-#Challange 3
+import sys
+
+class Rucksack:
+    def __init__ (self, items):
+        self.items = items
+        self.compartments1, self.compartments2 = items[:len(items)//2], items[len(items)//2:]
+        self.error = 0
+        self.priorities = 0
+
+    def findError(self):
+        e = list(set(self.compartments1) & set(self.compartments2))
+        self.error = e[0]
+        if self.error.isupper():
+            self.priorities = ord(self.error) - 38
+        else:
+            self.priorities = ord(self.error) - 96
+
+
+
+file = open('input.txt', 'r')
+lines = file.readlines()
+
+total = 0
+
+for line in lines:
+    line = line.rstrip('\n')
+    r = Rucksack(line)
+    r.findError()
+    total = total + r.priorities
+
+#Part 1 result
+print(total)
+
+exit()
+
+
 
