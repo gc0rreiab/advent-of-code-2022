4112d9752e18286aeb42d830b48bca4041cdea2b
gcb <gabriel.brito.95@gmail.com>
2022-12-19 12:44:13 +0000

    Add part 1 solution

--- day6_Tuning-Trouble/challenge.py
+++ day6_Tuning-Trouble/challenge.py
@@ -1,1 +1,18 @@
-#Python code for day 6
+import sys
+
+file = open('input.txt', 'r')
+line = file.readline().rstrip('\n')
+
+start_of_packet_marker = ['', '', '', '']
+i = 0
+for c in list(line):
+    i = i + 1
+    start_of_packet_marker.pop(0)
+    start_of_packet_marker.append(c)    
+    if len(set(start_of_packet_marker)) == len(start_of_packet_marker):
+        break
+
+#Start-Packet-Marker
+print(start_of_packet_marker)
+#Part 1 Solution
+print(i)
