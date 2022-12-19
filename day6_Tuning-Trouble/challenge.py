f3aa73307bf774f244cd14f28da66dee659712d4
gcb <gabriel.brito.95@gmail.com>
2022-12-19 13:01:46 +0000

    Add part 2 solution

--- day6_Tuning-Trouble/challenge.py
+++ day6_Tuning-Trouble/challenge.py
@@ -1,18 +1,37 @@
 import sys
 
+class Marker:
+
+    def __init__(self):
+        self.datastream_buffer = ''
+        self.marker = []
+        self.charsToBeProcessed = 0
+    
+    def checkStartMarker(self, datastream_buffer, n):
+        start_of_packet_marker = [''] * n
+        i = 0
+        for c in list(datastream_buffer):
+            i = i + 1
+            start_of_packet_marker.pop(0)
+            start_of_packet_marker.append(c)    
+            if len(set(start_of_packet_marker)) == len(start_of_packet_marker):
+                break
+        self.charsToBeProcessed = i
+        self.marker = start_of_packet_marker
+
+
 file = open('input.txt', 'r')
-line = file.readline().rstrip('\n')
+datastream_buffer = file.readline().rstrip('\n')
 
-start_of_packet_marker = ['', '', '', '']
-i = 0
-for c in list(line):
-    i = i + 1
-    start_of_packet_marker.pop(0)
-    start_of_packet_marker.append(c)    
-    if len(set(start_of_packet_marker)) == len(start_of_packet_marker):
-        break
+m = Marker()
 
-#Start-Packet-Marker
-print(start_of_packet_marker)
-#Part 1 Solution
-print(i)
+#Uncomment for Part 1 Solution (start-of-packet marker)
+m.checkStartMarker(datastream_buffer, 4)
+
+#Part 2 Solution (start-of-message marker)
+m.checkStartMarker(datastream_buffer, 14)
+
+#Start-Marker
+print(m.marker)
+#Solution
+print(m.charsToBeProcessed)
